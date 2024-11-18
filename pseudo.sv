b6ebd9889371898a9f6dea5a87183953397d5b4f
unknown <harshavardhanreddy527@gmail.com>
2024-11-17 18:04:24 -0800

    Added test plan

--- pseudo.sv
+++ pseudo.sv
@@ -10,6 +10,21 @@ class Node;
 endclass
 
 class BT;
+  function automatic Node traverse_and_evict(Node root, ref int array[4], ref int count);
+    if (root != null && count < $size(array)) begin
+      if (root.value == 0) begin
+        array[count] = 1;
+        count += 1;
+        root.right = traverse_and_evict(root.right, array, count);
+      end else begin
+        array[count] = 0;
+        count += 1;
+        root.left = traverse_and_evict(root.left, array, count);
+      end
+    end
+    return root; 
+  endfunction
+
   function automatic Node inorder_manipulate(int x[], ref int i, Node root);
     if (root != null) begin
       root.left = inorder_manipulate(x, i, root.left);
@@ -19,20 +34,21 @@ class BT;
     end
     return root;
   endfunction
+
   function automatic Node manipulate(int A[], int k, Node root);
-      if (root == null) begin
-        return root;
-      end else begin
-        root.value = A[k];
-        if (A[k] == 0) begin
-          root.left = manipulate(A, k + 1, root.left);
-        end else begin
-          root.right = manipulate(A, k + 1, root.right);
-        end
-      end
+    if (root == null) begin
       return root;
+    end else begin
+      root.value = A[k];
+      if (A[k] == 0) begin
+        root.left = manipulate(A, k + 1, root.left);
+      end else begin
+        root.right = manipulate(A, k + 1, root.right);
+      end
+    end
+    return root;
   endfunction
-      
+
   function void inorder(Node root);
     if (root != null) begin  
       inorder(root.left);         
@@ -40,17 +56,19 @@ class BT;
       inorder(root.right);  
     end 
   endfunction
-
 endclass
 
 module sample;
   initial begin
     int j = 0; 
-    int k=0;
-    int A[4]='{0,1,0,1};
+    int k = 0;
+    int count = 0;
+    int array[4];
+    int A[4] = '{0, 1, 0, 1};
     int x[16] = '{1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 1}; 
     Node root;
     BT bt = new();
+
     root = new(0);
     root.left = new(0);
     root.right = new(0);
@@ -74,9 +92,14 @@ module sample;
     $display("Tree after appending x array (Inorder Traversal):");
     bt.inorder(root);
 
-    root=bt.manipulate(A,k,root);
-    $display("Root value after appending A array:");
+    root = bt.manipulate(A, k, root);
+    $display("Tree after appending A array (Inorder Traversal):");
     bt.inorder(root);
 
+    count = 0;
+    root = bt.traverse_and_evict(root, array, count);
+
+    $display("Evicted Array:%p",array);
+    
   end
 endmodule
