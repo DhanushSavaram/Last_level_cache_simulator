151d925fe1731a8e49574b21ebf299257ff21bdc
DUVVURU HARSHA VARDHAN <harshavardhanreddy527@gmail.com>
2024-12-01 17:18:57 -0800

    Update cache_define.sv

--- cache_define.sv
+++ cache_define.sv
@@ -1,39 +1,41 @@
 package cache_define;
 
+parameter SILENT_MODE = 0, NORMAL_MODE = 1;
+
 //Snoop Results of other caches
-`define NOHIT 2'b11
-`define HIT 2'b00
-`define HITM 2'b01
+parameter NOHIT = 2'b11;
+parameter HIT = 2'b00;
+parameter HITM = 2'b01;
 
 //"---00= HIT,   ---01 = HitM        ---10   or  ---11 = NoHit"
 
-`define READ 1
-`define WRITE 2
-`define INVALIDATE 3
-`define RWIM 4 
+parameter READ = 1;
+parameter WRITE = 2;
+parameter INVALIDATE = 3;
+parameter RWIM = 4; 
 
  //L2 to L1 Messages
-`define GETLINE 1 
-`define SENDLINE 2 
-`define INVALIDATELINE 3 
-`define EVICTLINE 4 
+parameter GETLINE = 1;
+parameter SENDLINE = 2; 
+parameter INVALIDATELINE = 3;
+parameter EVICTLINE = 4; 
 
 //MESI bits
-`define I 2'b00
-`define E 2'b01
-`define M 2'b10
-`define S 2'b11
+parameter I = 2'b00;
+parameter E = 2'b01;
+parameter M = 2'b10;
+parameter S = 2'b11;
 
-PARAMETER CACHE_WIDTH = 32;
-PARAMETER WAYS = 16;          
-PARAMETER OFFSET_BITS = 6;
-PARAMETER CACHE_SIZE = 2**CACHE_WIDTH;
-PARAMETER CACHE_LINE_SIZE = 2**OFFSET_BITS;
-PARAMETER TOTAL_LINES = CACHE_SIZE / CACHE_LINE_SIZE;
+parameter CACHE_WIDTH = 32;
+parameter WAYS = 16;          
+parameter OFFSET_BITS = 6;
+parameter CACHE_SIZE = 2**CACHE_WIDTH;
+parameter CACHE_LINE_SIZE = 2**OFFSET_BITS;
+parameter TOTAL_LINES = CACHE_SIZE / CACHE_LINE_SIZE;
 
-PARAMETER SETS = 2**(TOTAL_LINES/WAYS);          
-PARAMETER INDEX_BITS = $clog2(SETS);
-PARAMETER PLRU_BITS = WAYS-1;
+parameter SETS = 2**(TOTAL_LINES/WAYS);          
+parameter INDEX_BITS = $clog2(SETS);
+parameter PLRU_BITS = WAYS-1;
 
 typedef struct packed {
   bit [1:0]MESI;           
@@ -45,4 +47,3 @@ typedef struct packed {
 // typedef enum [1:0]{GETLINE, SENDLINE, INVALIDATELINE, EVICTLINE} L2_to_L1;
 
 endpackage
-
